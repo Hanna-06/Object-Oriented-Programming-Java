ac04e94f8d2443edacc7f6002ee1a3d5e6ab1d33
Hanna-06 <hannasusan06@gmail.com>
2025-05-28 00:55:10 -0700

    Create Stringtoken.java

---
+++ Stringtoken.java
@@ -0,0 +1,24 @@
+//Java program to use string tokenizer.
+
+import java.util.*; 
+public class Stringtoken 
+{ 
+	public static void main(String args[])  
+	{ 
+		int n; 
+		int sum=0; 
+		Scanner sc=new Scanner(System.in); 
+		System.out.println("Enter integers with one space gap:"); 
+		String s=sc.nextLine(); 
+		StringTokenizer st=new StringTokenizer(s," "); 
+		while (st.hasMoreTokens())  
+		{ 
+			String temp=st.nextToken(); 
+			n=Integer.parseInt(temp); 
+			System.out.println(n); 
+			sum=sum+n; 
+		} 
+		System.out.println("Sum of the integers is: "+sum); 
+		sc.close(); 
+	} 
+} 
